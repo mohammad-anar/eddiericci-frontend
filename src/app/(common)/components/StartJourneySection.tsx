e8171bcd09966db78c98324df452ad342e62daad
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-04-11 04:50:59 +0600

    start journey section modified

--- src/app/(common)/components/StartJourneySection.tsx
+++ src/app/(common)/components/StartJourneySection.tsx
@@ -9,10 +9,10 @@ const StartJourneySection = () => {
       <div className="absolute">
         <Image src={image} alt="Start Journey" />
       </div>
-      <div className="container min-h-[400px] flex items-center justify-center relative z-10 w-full h-full">
+      <div className="container py-32 flex items-center justify-center relative z-10 w-full h-full">
         <div className="space-y-4 flex flex-col items-center text-center">
-          <h3 className="text-2xl lg:text-4xl font-bold font-heading text-white">Ready to Start Your Journey?</h3>
-          <p className="text-gray-100">Join thousands of players, coaches, and clubs who are already <br/> building their future on K10 Football</p>
+          <h3 className="text-2xl sm:text-3xl lg:text-4xl font-bold font-heading text-white">Ready to Start Your Journey?</h3>
+          <p className="text-gray-100 sm:text-[16px]">Join thousands of players, coaches, and clubs who are already <br/> building their future on K10 Football</p>
           <GradientButton text="Get Started Today" />
         </div>
       </div>
